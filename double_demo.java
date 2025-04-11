ec5516c968eca0bafd6fc270790afedd4e2516ce
drushti <drushtigowda13@gmail.com>
2025-04-11 20:17:33 +0530

    Add files via upload

---
+++ double_demo.java
@@ -0,0 +1,16 @@
+class double_demo
+{
+	public static void main(String args[])
+	{
+			int x[][]={{10,40,67},{56,90,78}};
+			int i,j;
+			for(i=0;i<2;i++)
+			{
+				for(j=0;j<3;j++)
+				{
+					System.out.print("\t"+x[i][j]);
+				}
+				System.out.println();
+			}
+	}
+}
